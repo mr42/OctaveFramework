0bde6b3b4ef285fa778b6c8919f234f8c183f4b3
mr42 <rudolphmichael42@gmail.com>
2014-11-13 13:15:24 +0100

    exercise1

---
+++ octave/t2v.m
@@ -0,0 +1,5 @@
+function [p] = t2v(M)
+
+p = [M(1,3), M(2,3), acos(M(1,1))];
+
+end
---
+++ octave/v2t.m
@@ -0,0 +1,7 @@
+function [M] = v2t(p)
+
+M = [cos(p(3)), -sin(p(3)), p(1);
+     sin(p(3)), cos(p(3)), p(2);
+     0, 0, 1];
+
+end
